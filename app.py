e484ba925303d43fa1948d6cd8e568afa0eb1791
Mayukh Mondal <21051405@kiit.ac.in>
2023-05-19 23:09:24 +0530

    wsgi

--- app.py
+++ app.py
@@ -158,5 +158,7 @@ def register():
 def login():
     return render_template('pages-login.html')
 
+host = "0.0.0.0"
+port = 8080
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host,port,debug=False)
