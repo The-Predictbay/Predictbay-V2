0f96c3a09a38e92bb14c4556586758fd20019928
Mayukh Mondal <21051405@kiit.ac.in>
2023-05-22 04:18:47 +0530

    error handled

--- app.py
+++ app.py
@@ -45,6 +45,17 @@ from queue import Queue
 
 app = Flask(__name__)
 
+class InvalidTickerError(Exception):
+    pass
+def get_data(ticker, period):
+    try:
+        df = yf.download(ticker, period=period)
+        if df.empty:
+            raise InvalidTickerError(f"Invalid ticker: {ticker}")
+        return df
+    except Exception as e:
+        raise InvalidTickerError(f"Invalid ticker: {ticker}") from e
+
 def Data_fetch_transform(data):
     # data = yf.download(ticker)
     data['Date'] = pd.to_datetime(data.index, infer_datetime_format=True)
@@ -125,131 +136,134 @@ def index():
     else:
         ticker = 'AAPL'
     
+    try:
+        period = '10y'
+        df = get_data(ticker, period)
+
+        closing_prices = df['Close']
+
+        high_value = get_today_high(ticker)
+        increase_status_high, percentage_change_high = get_percentage_change_high(ticker)
+        close_value = get_today_close(ticker)
+        increase_status_Close, percentage_change_Close = get_percentage_change_Close(ticker)
+        open_value = get_today_open(ticker)
+        increase_status_Open, percentage_change_Open = get_percentage_change_Open(ticker)
+
+        chart_data = [{'x': str(date), 'y': price} for date, price in closing_prices.items()]
+        ma100 = closing_prices.rolling(window=100).mean()
+        ma100 = [{'x': str(date), 'y': price} for date, price in ma100.items() if not pd.isna(price)]
+        ma200 = closing_prices.rolling(window=200).mean()
+        ma200 = [{'x': str(date), 'y': price} for date, price in ma200.items() if not pd.isna(price)]
+
+        data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.7)])
+        data_testing = pd.DataFrame(df['Close'][int(len(df)*0.7):int(len(df))])
+
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        data_training_array = scaler.fit_transform(data_training)
+
+        x_train = []
+        y_train = []
+
+        for i in range(100, data_training_array.shape[0]):
+            x_train.append(data_training_array[i-100: i])
+            y_train.append(data_training_array[i, 0])
+
+        x_train, y_train = np.array(x_train), np.array(y_train)
+
+        # Load model
+        model = load_model('lstm_1000_epochs.h5')
+
+        result_queue = Queue()
+
+        # Creating thread for model prediction
+        bilstm_thread = threading.Thread(target=biLSTM, args=(df, result_queue))
+        bilstm_thread.start()
+
+        
+
+        past_100_days = data_training.tail(100)
+        final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+
+        input_data = scaler.fit_transform(final_df)
+
+        x_test = []
+        y_test = []
+
+        for i in range(100, input_data.shape[0]):
+            x_test.append(input_data[i-100:i])
+            y_test.append(input_data[i, 0])
+
+        x_test, y_test = np.array(x_test), np.array(y_test)
+
+        y_predict = model.predict(x_test)
+
+        scaler = scaler.scale_
+
+        scale_factor = 1/scaler[0]
+        y_predict = y_predict * scale_factor
+        y_test = y_test * scale_factor
+
+        fig2 = go.Figure()
+        fig2.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=y_test, name='Original Price'))
+        fig2.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=y_predict[:, 0], name='Predict'))
+        fig2.update_layout(
+                        xaxis_title='Date',
+                        yaxis_title='Price',
+                        height=500 ,
+                        plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
+                        xaxis=dict(showticklabels=False)
+                        )
+        graph_html = fig2.to_html(full_html=False)
+
+        last_100_days = data_testing[-100:].values
+        scaler = MinMaxScaler()
+        last_100_days_scaled = scaler.fit_transform(last_100_days)
+
+        predicted_prices = []
+
+        # Wait for the thread to finish
+        bilstm_thread.join()
+        
+        biLSTM_predicted_price, predictions_biLSTM, biLSTM_ytest = result_queue.get()
+
+        fig3 = go.Figure()
+        fig3.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=biLSTM_ytest, name='Original Price'))
+        fig3.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=predictions_biLSTM[:, 0], name='Predict'))
+        fig3.update_layout(
+                        xaxis_title='Date',
+                        yaxis_title='Price',
+                        height=500 ,
+                        plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
+                        xaxis=dict(showticklabels=False)
+                        )
+        bilstm_graph_html = fig3.to_html(full_html=False)
 
 
-    period = '10y'
-    df = yf.download(ticker, period=period)
+        for i in range(1):
+            X_test = np.array([last_100_days_scaled])
+            X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+            predicted_price = model.predict(X_test)
+            predicted_prices.append(predicted_price)
+            last_100_days_scaled = np.append(last_100_days_scaled, predicted_price)
+            last_100_days_scaled = np.delete(last_100_days_scaled, 0)
 
-    closing_prices = df['Close']
+        predicted_prices = np.array(predicted_prices)
+        predicted_prices = predicted_prices.reshape(predicted_prices.shape[0], predicted_prices.shape[2])
+        predicted_prices = scaler.inverse_transform(predicted_prices)
+        predicted_price = predicted_prices[0][0]
+        
+        if(biLSTM_predicted_price > predicted_price):
+            uprange = floor(biLSTM_predicted_price)
+            downrange = floor(predicted_price)
+        else:
+            uprange = floor(predicted_price)
+            downrange = floor(biLSTM_predicted_price)
 
-    high_value = get_today_high(ticker)
-    increase_status_high, percentage_change_high = get_percentage_change_high(ticker)
-    close_value = get_today_close(ticker)
-    increase_status_Close, percentage_change_Close = get_percentage_change_Close(ticker)
-    open_value = get_today_open(ticker)
-    increase_status_Open, percentage_change_Open = get_percentage_change_Open(ticker)
+         
+        return render_template('index.html', ticker=ticker, chart_data=chart_data, predicted_price=round(predicted_price, 2), biLSTM_predicted_price=round(biLSTM_predicted_price, 2), uprange = uprange, downrange = downrange, bilstm_graph_html = bilstm_graph_html, ma100=ma100,ma200=ma200, graph_html=graph_html,high_value=high_value,close_value=close_value,open_value=open_value,high_status=increase_status_high,high_percent=percentage_change_high,Close_status=increase_status_Close,Close_percent=percentage_change_Close,Open_status=increase_status_Open,Open_percent=percentage_change_Open)
+    except InvalidTickerError as e:
+        return render_template('errorpage.html')
 
-    chart_data = [{'x': str(date), 'y': price} for date, price in closing_prices.items()]
-    ma100 = closing_prices.rolling(window=100).mean()
-    ma100 = [{'x': str(date), 'y': price} for date, price in ma100.items() if not pd.isna(price)]
-    ma200 = closing_prices.rolling(window=200).mean()
-    ma200 = [{'x': str(date), 'y': price} for date, price in ma200.items() if not pd.isna(price)]
-
-    data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.7)])
-    data_testing = pd.DataFrame(df['Close'][int(len(df)*0.7):int(len(df))])
-
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    data_training_array = scaler.fit_transform(data_training)
-
-    x_train = []
-    y_train = []
-
-    for i in range(100, data_training_array.shape[0]):
-        x_train.append(data_training_array[i-100: i])
-        y_train.append(data_training_array[i, 0])
-
-    x_train, y_train = np.array(x_train), np.array(y_train)
-
-    # Load model
-    model = load_model('lstm_1000_epochs.h5')
-
-    result_queue = Queue()
-
-    # Creating thread for model prediction
-    bilstm_thread = threading.Thread(target=biLSTM, args=(df, result_queue))
-    bilstm_thread.start()
-
-    
-
-    past_100_days = data_training.tail(100)
-    final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
-
-    input_data = scaler.fit_transform(final_df)
-
-    x_test = []
-    y_test = []
-
-    for i in range(100, input_data.shape[0]):
-        x_test.append(input_data[i-100:i])
-        y_test.append(input_data[i, 0])
-
-    x_test, y_test = np.array(x_test), np.array(y_test)
-
-    y_predict = model.predict(x_test)
-
-    scaler = scaler.scale_
-
-    scale_factor = 1/scaler[0]
-    y_predict = y_predict * scale_factor
-    y_test = y_test * scale_factor
-
-    fig2 = go.Figure()
-    fig2.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=y_test, name='Original Price'))
-    fig2.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=y_predict[:, 0], name='Predict'))
-    fig2.update_layout(
-                    xaxis_title='Date',
-                    yaxis_title='Price',
-                    height=500 ,
-                    plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
-                    xaxis=dict(showticklabels=False)
-                    )
-    graph_html = fig2.to_html(full_html=False)
-
-    last_100_days = data_testing[-100:].values
-    scaler = MinMaxScaler()
-    last_100_days_scaled = scaler.fit_transform(last_100_days)
-
-    predicted_prices = []
-
-    # Wait for the thread to finish
-    bilstm_thread.join()
-    
-    biLSTM_predicted_price, predictions_biLSTM, biLSTM_ytest = result_queue.get()
-
-    fig3 = go.Figure()
-    fig3.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=biLSTM_ytest, name='Original Price'))
-    fig3.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=predictions_biLSTM[:, 0], name='Predict'))
-    fig3.update_layout(
-                    xaxis_title='Date',
-                    yaxis_title='Price',
-                    height=500 ,
-                    plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
-                    xaxis=dict(showticklabels=False)
-                    )
-    bilstm_graph_html = fig3.to_html(full_html=False)
-
-
-    for i in range(1):
-        X_test = np.array([last_100_days_scaled])
-        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-        predicted_price = model.predict(X_test)
-        predicted_prices.append(predicted_price)
-        last_100_days_scaled = np.append(last_100_days_scaled, predicted_price)
-        last_100_days_scaled = np.delete(last_100_days_scaled, 0)
-
-    predicted_prices = np.array(predicted_prices)
-    predicted_prices = predicted_prices.reshape(predicted_prices.shape[0], predicted_prices.shape[2])
-    predicted_prices = scaler.inverse_transform(predicted_prices)
-    predicted_price = predicted_prices[0][0]
-    
-    if(biLSTM_predicted_price > predicted_price):
-        uprange = floor(biLSTM_predicted_price)
-        downrange = floor(predicted_price)
-    else:
-        uprange = floor(predicted_price)
-        downrange = floor(biLSTM_predicted_price)
-
-    return render_template('index.html', ticker=ticker, chart_data=chart_data, predicted_price=round(predicted_price, 2), biLSTM_predicted_price=round(biLSTM_predicted_price, 2), uprange = uprange, downrange = downrange, bilstm_graph_html = bilstm_graph_html, ma100=ma100,ma200=ma200, graph_html=graph_html,high_value=high_value,close_value=close_value,open_value=open_value,high_status=increase_status_high,high_percent=percentage_change_high,Close_status=increase_status_Close,Close_percent=percentage_change_Close,Open_status=increase_status_Open,Open_percent=percentage_change_Open)
 
 # Function to get today's high value of a stock
 def get_today_high(symbol):
